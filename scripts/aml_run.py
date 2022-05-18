4162a92b5bb16162d4eceecf60df826378a49ff7
Hossein Sarshar <hossein.sarshar@gmail.com>
2022-05-18 19:38:24 +0000

    cleared the putputs

--- scripts/aml_run.py
+++ scripts/aml_run.py
@@ -13,9 +13,10 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, OrdinalEncoder, LabelEncoder
 from sklearn.compose import make_column_transformer
 from sklearn.linear_model import LogisticRegression
-# from deltalake import DeltaTable
+from azureml.core.model import Model
+from azureml.core.resource_configuration import ResourceConfiguration
 
-from azureml.core import Run
+from azureml.core import Dataset, Run
 from packaging.version import Version
 if Version(sklearnver) < Version("0.23.0"):
     from sklearn.externals import joblib
@@ -41,62 +42,57 @@ def featurize_data(preped_data):
     X_features = ct.fit_transform(X)
     return X_features, y_train
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--data_folder', type=str, dest='data_folder', default='data', help='data folder mounting point')
-parser.add_argument('--model_name', type=str, dest='model_name', default='data', help='data folder mounting point')
-parser.add_argument('--featureset_name', type=str, dest='featureset_name', default='data', help='data folder mounting point')
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_folder', type=str, dest='data_folder', default='data', help='data folder mounting point')
+    parser.add_argument('--model_name', type=str, dest='model_name', default='data', help='data folder mounting point')
+    parser.add_argument('--featureset_name', type=str, dest='featureset_name', default='data', help='data folder mounting point')
+    args = parser.parse_args()
 
-run = Run.get_context()
-parent_id = run.parent.id
+    run = Run.get_context()
+    parent_id = run.parent.id
 
-print('run.input_datasets')
-print(run.input_datasets)
+    print('run.input_datasets')
+    print(run.input_datasets)
 
-print('args.data_folder')
-print(args.data_folder)
+    print('args.data_folder')
+    print(args.data_folder)
 
-dataset = run.input_datasets['output']
-print("type(dataset)")
-print(type(dataset))
+    dataset = run.input_datasets['output']
+    print("type(dataset)")
+    print(type(dataset))
 
-print('os.listdir(dataset)')
-print(os.listdir(dataset))
+    print('os.listdir(dataset)')
+    print(os.listdir(dataset))
 
-data_folder = args.data_folder
+    data_folder = args.data_folder
 
-print(f"data_folder: [{data_folder}]")
+    print(f"data_folder: [{data_folder}]")
 
-print("reading from parquet")
-preped_data = pd.read_parquet(data_folder)
+    print("reading from parquet")
+    preped_data = pd.read_parquet(data_folder)
 
-X_features, y_train = featurize_data(preped_data)
+    X_features, y_train = featurize_data(preped_data)
 
-X_features, y_train = featurize_data(preped_data)
+    log_reg = LogisticRegression(solver='liblinear')
+    log_reg.fit(X_features, y_train)
 
-log_reg = LogisticRegression(solver='liblinear')
-log_reg.fit(X_features, y_train)
+    model_file_name = 'titanic.pkl'
+    file_path = os.path.join('./outputs/', model_file_name)
 
-model_file_name = 'titanic.pkl'
-file_path = os.path.join('./outputs/', model_file_name)
+    os.makedirs('./outputs/', exist_ok=True)
+    # save model in the outputs folder so it automatically get uploaded
+    with open(model_file_name, "wb") as file:
+        joblib.dump(value=log_reg, filename=file_path)
 
-os.makedirs('./outputs/', exist_ok=True)
-# save model in the outputs folder so it automatically get uploaded
-with open(model_file_name, "wb") as file:
-    joblib.dump(value=log_reg, filename=file_path)
+    ws = run.experiment.workspace
 
-from azureml.core.model import Model
-from azureml.core.resource_configuration import ResourceConfiguration
+    ds_feature = Dataset.get_by_name(ws, name=args.featureset_name)
 
-ws = run.experiment.workspace
-from azureml.core import Dataset
-
-ds_feature = Dataset.get_by_name(ws, name=args.featureset_name)
-
-model = Model.register(model_path=file_path,
-                        model_name=args.model_name,
-                        datasets=[('featurized data', ds_feature)],
-                        tags={'run_id': parent_id},
-                        description="Ridge regression model to predict diabetes",
-                        resource_configuration=ResourceConfiguration(cpu=1, memory_in_gb=0.5),
-                        workspace=ws)
+    model = Model.register(model_path=file_path,
+                            model_name=args.model_name,
+                            datasets=[('featurized data', ds_feature)],
+                            tags={'run_id': parent_id},
+                            description="Ridge regression model to predict diabetes",
+                            resource_configuration=ResourceConfiguration(cpu=1, memory_in_gb=0.5),
+                            workspace=ws)
